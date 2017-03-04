e35a3f215a12a8bd2791141670f2ce442d55bfcc
Yiwen Zhang <yiwenzhg@umich.edu>
2017-03-04 15:37:31 -0500

    add accurate model & measure for small flow

---
+++ WRITE_compare_accurate/ibverbs/ibv_client/rdma-common.h
@@ -0,0 +1,38 @@
+#ifndef RDMA_COMMON_H
+#define RDMA_COMMON_H
+
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <rdma/rdma_cma.h>
+#include <sys/time.h>
+#include <time.h>
+#include "get_clock.h"
+#include <errno.h>
+
+#define TEST_NZ(x) do { if ( (x)) { printf("ret = %d; ", x);  die("error: " #x " failed (returned non-zero)." );} } while (0)
+#define TEST_Z(x)  do { if (!(x)) die("error: " #x " failed (returned zero/null)."); } while (0)
+
+enum mode {
+  M_WRITE,
+  M_READ
+};
+
+void die(const char *reason);
+
+void build_connection(struct rdma_cm_id *id);
+void build_params(struct rdma_conn_param *params);
+void destroy_connection(void *context);
+//void * get_local_message_region(void *context);
+void on_connect(void *context);
+//void send_mr(void *context);
+void register_MR(void *context);
+void run_iter(void *context);
+void send_sz(void *context);
+void set_mode(enum mode m);
+void checkpoint(int i);
+void measure_time(const char * filename, cycles_t START_cycle);
+
+#endif
