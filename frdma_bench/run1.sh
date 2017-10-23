e45768b5d036182d8d46e7e9b393edbd2945cf89
yiwenzhg <yiwenzhg@cp-1.yiwenzhg-qv29452.gaia-pg0.apt.emulab.net>
2017-10-22 21:10:15 -0600

    update batch code

--- frdma_bench/run1.sh
+++ frdma_bench/run1.sh
@@ -3,4 +3,8 @@
 #./write_bw 192.168.0.28 -F -s 1000000 -n 1000000 -O 0 -e -o 1M_vs_1G_10000_A.txt
 #./write_bw 192.168.0.28 -F -s 1000000 -n 1000000 -O 0 -e -o 1M_vs_1G_1000_A.txt
 #./write_bw 192.168.0.28 -F -s 1000000000 -n 1000 -O 0 -e -o 1G_1000_vs_1M_A.txt
-./write_bw 192.168.0.28 -F -s 1000000000 -n 1000 -O 0 -e -o temp
+#./write_bw 192.168.0.28 -F -s 1000000000 -n 1000 -O 0 -e -o temp
+#./write_bw 192.168.0.28 -F -s 1000000000 -n 1000 -O 0 -e -o new_1G_vs_1M_t1000_A.txt
+#./write_bw 192.168.0.28 -F -s 1000000 -n 1000000 -O 0 -e -o new_1M_vs_1M_t1000_A.txt
+#./write_bw 192.168.0.28 -F -s 1000000000 -n 1000 -O 0 -e -o new_1G_split_vs_1M_t1000_A.txt
+./write_bw 192.168.0.28 -F -s 1000000000 -n 1000 -O 0 -e -o new_1G_split_vs_1M_t1_A.txt
