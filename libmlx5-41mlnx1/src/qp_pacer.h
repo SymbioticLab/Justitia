731699c52df550c7be61f6422eead754fc4bb31b
Yiwen Zhang <yiwenzhg@umich.edu>
2018-01-31 17:40:00 -0500

    add header

---
+++ libmlx5-41mlnx1/src/qp_pacer.h
@@ -0,0 +1,36 @@
+#ifndef QP_PACER_H
+#define QP_PACER_H
+
+#include "pacer.h"
+
+static void contact_pacer_read() {
+    /* prepare unix domain socket */
+    unsigned int s, len;
+    struct sockaddr_un remote;
+    char str[MSG_LEN];
+
+    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    printf("Contacting pacer...\n");
+
+    remote.sun_family = AF_UNIX;
+    strcpy(remote.sun_path, SOCK_PATH);
+    len = strlen(remote.sun_path) + sizeof(remote.sun_family);
+    if (connect(s, (struct sockaddr *)&remote, len) == -1) {
+        perror("connect");
+        exit(1);
+    }
+   
+    printf("Sending read message...\n");
+    strcpy(str, "read");
+    if (send(s, str, strlen(str), 0) == -1) {
+        perror("send: read");
+        exit(1);
+    }
+
+    close(s);
+}
+#endif
