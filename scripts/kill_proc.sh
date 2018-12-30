363e0103829219a52b2a930691a2d2d2bdd6a005
Yiwen Zhang <yiwenzhg@umich.edu>
2018-12-30 16:59:39 -0500

    add a useful script to kill process

---
+++ scripts/kill_proc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# assume there is a file called "nodes" in cmd which includes all the node hostname
+proc="lock_simulation"
+for node in $(cat nodes); do
+  echo "killing $proc process in $node..."
+  pids=`ssh -o 'StrictHostKeyChecking no' -p 22 $node ps -ef | grep $proc | awk '{print $2}'`
+  for pid in $pids; do
+    echo $pid
+    ssh -o "StrictHostKeyChecking no" -p 22 $node "sudo kill -9 $pid"  &
+  done
+done
+wait
+echo "DONE"
