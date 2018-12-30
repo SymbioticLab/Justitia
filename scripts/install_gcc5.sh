18601762fe99de1540e9d08cc87b109404118a44
Yiwen Zhang <yiwenzhg@umich.edu>
2018-12-29 19:08:16 -0500

    tweaks in scripts

--- scripts/install_gcc5.sh
+++ scripts/install_gcc5.sh
@@ -2,13 +2,7 @@
 # on cloudlab, do not run as root
 for node in $(cat nodes); do
   echo "installing gcc5 in $node..."
-  ssh -o "StrictHostKeyChecking no" -p 22 $node "sudo apt-get -y update"
-  ssh -o "StrictHostKeyChecking no" -p 22 $node "sudo apt-get -y install software-properties-common"
-  ssh -o "StrictHostKeyChecking no" -p 22 $node "sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test"
-  ssh -o "StrictHostKeyChecking no" -p 22 $node "sudo apt-get -y update"
-  ssh -o "StrictHostKeyChecking no" -p 22 $node "sudo apt-get install -y gcc-5 g++-5"
-  ssh -o "StrictHostKeyChecking no" -p 22 $node "sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-
-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5"
+  ssh -o "StrictHostKeyChecking no" -p 22 $node "sudo apt-get -y update && sudo apt-get -y install software-properties-common && sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test && sudo apt-get -y update && sudo apt-get install -y gcc-5 g++-5 && sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5"
 done
 wait
 echo "DONE"
--- scripts/upgrade_mlnx.sh
+++ scripts/upgrade_mlnx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for node in $(cat nodes); do
-  echo "installing openmpi in $node..."
+  echo "upgrade mlnx driver in $node..."
   ssh -o "StrictHostKeyChecking no" -p 22 $node "sudo /proj/gaia-PG0/yiwen/MLNX_OFED_LINUX-4.2-1.0.0.0-ubuntu14.04-x86_64/mlnxofedinstall --force && sudo /proj/gaia-PG0/yiwen/MLNX_OFED_LINUX-4.2-1.0.0.0-ubuntu14.04-x86_64/etc/init.d/openibd restart" &
 done
 wait
