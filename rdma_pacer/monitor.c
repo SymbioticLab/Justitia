d3f863b6564f00f8bec644767296465d31a48c0e
Yiwen Zhang <yiwenzhg@umich.edu>
2019-01-18 04:22:20 -0500

    add an option to set different improve factor when split level is is found or target is met (still set to the same 0.25 now)

--- rdma_pacer/monitor.c
+++ rdma_pacer/monitor.c
@@ -27,6 +27,7 @@
 //#define CMH_PERCENTILE  0.99    // pencentile ask from CMH
 #define CMH_PERCENTILE  0.99    // pencentile ask from CMH
 #define improvement_factor 0.25  // improvement of current measured tail over previous tail that needs to be achieved to keep the current num_split_qps
+#define elasticity_factor 0.25  // same as improvement_factor, but used after a level is found or a target is met
 
 CMH_type *cmh = NULL;
 
@@ -318,7 +319,7 @@ void monitor_latency(void *arg)
                                     found_split_level = 0;
 
                                 } else {
-                                    if (num_split_qps > 1) {
+                                    if (num_split_qps > 1) {    // actually unnecessary to check > 1 here.
                                         printf("Elapsed time is %.2f us; num_split_qps = %d does not perform well with %.2f improvement. (prev, curr) = (%.2f, %.2f). Decreae num_split_qps back to %d and stays there\n",
                                             (target_unmet_counter_end - target_unmet_counter_start) / cpu_mhz, num_split_qps, (prev_tail - measured_tail)/prev_tail, prev_tail, measured_tail, num_split_qps - 1);
                                         num_split_qps--;
@@ -334,7 +335,7 @@ void monitor_latency(void *arg)
                             }
                         } else {    // if has stabalized at a split level
                             if (measured_tail < prev_tail) {    // better than before but still above target
-                                if ((prev_tail - measured_tail)/prev_tail > improvement_factor) {
+                                if ((prev_tail - measured_tail)/prev_tail > elasticity_factor) {
                                     if (num_split_qps > 1) {
                                         printf("After found a level; num_split_qps = %d performs well with %.2f improvement. (prev, curr) = (%.2f, %.2f). Decrease num_split_qps to %d to minimize cost\n",
                                             num_split_qps, (prev_tail - measured_tail)/prev_tail, prev_tail, measured_tail, num_split_qps - 1);
@@ -345,7 +346,7 @@ void monitor_latency(void *arg)
                                 }
 
                             } else {    // worse than before and still above target
-                                if ((measured_tail - prev_tail)/prev_tail > improvement_factor) {
+                                if ((measured_tail - prev_tail)/prev_tail > elasticity_factor) {
                                     if (num_split_qps < MAX_NUM_SPLIT_QPS) {
                                         printf("After found a level; num_split_qps = %d does not perform well with %.2f degradation. (prev, curr) = (%.2f, %.2f). Increase num_split_qps to %d to improve isolation\n",
                                             num_split_qps, (prev_tail - measured_tail)/prev_tail, prev_tail, measured_tail, num_split_qps + 1);
@@ -394,7 +395,7 @@ void monitor_latency(void *arg)
                         num_split_qps = __atomic_load_n(&cb.sb->num_active_split_qps, __ATOMIC_RELAXED);    // shouldn't need to load from mem. Just to be safe.
 
                         if (measured_tail < prev_tail) {    // better than before and still below target
-                            if ((prev_tail - measured_tail)/prev_tail > improvement_factor) {
+                            if ((prev_tail - measured_tail)/prev_tail > elasticity_factor) {
                                 if (num_split_qps > 1) {
                                     printf("Target Met; num_split_qps = %d performs well with %.2f improvement. (prev, curr) = (%.2f, %.2f). Decrease num_split_qps to %d to minimize cost\n",
                                         num_split_qps, (prev_tail - measured_tail)/prev_tail, prev_tail, measured_tail, num_split_qps - 1);
@@ -405,7 +406,7 @@ void monitor_latency(void *arg)
                             }
 
                         } else {    // worse than before but still below target
-                            if ((measured_tail - prev_tail)/prev_tail > improvement_factor) {
+                            if ((measured_tail - prev_tail)/prev_tail > elasticity_factor) {
                                 if (num_split_qps < MAX_NUM_SPLIT_QPS) {
                                     printf("After found a level; num_split_qps = %d does not perform well with %.2f degradation. (prev, curr) = (%.2f, %.2f). Increase num_split_qps to %d to improve isolation\n",
                                         num_split_qps, (prev_tail - measured_tail)/prev_tail, prev_tail, measured_tail, num_split_qps + 1);
