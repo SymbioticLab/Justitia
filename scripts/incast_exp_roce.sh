2c2c8bc6460c063a79ec398a0cc571a14ab2940e
Yiwen Zhang <yiwenzhg@umich.edu>
2019-01-27 05:26:10 -0500

    update incast exp script 8bw+1lat

--- scripts/incast_exp_roce.sh
+++ scripts/incast_exp_roce.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+## "nodes" file should not include the last receiving node
 ib_write_bw=/proj/rsc-PG0/yiwen/frdma/perftest-4.2/ib_write_bw
 ib_write_lat=/proj/rsc-PG0/yiwen/frdma/perftest-4.2/ib_write_lat
 out_dir=/tmp
 ip_base=192.168.1 # not used here
-ip_dst=192.168.1.2
+ip_dst=192.168.1.10
+receiver=node10
 bw_size=1000000
 lat_size=16
-bw_iters=100000
-lat_iter=100000
+bw_iters=20000
+lat_iter=10000
 port_base=8887
 ib_dev=mlx5_1
 gidx=3
 cnt=1
-
+num_senders=$(wc -l < nodes)
+#echo $num_senders
 if [ "$#" -ne 1 ]; then
   echo "Usage: bash $0.sh <sender/receiver>"
   exit
@@ -24,34 +27,36 @@ if [ $1 == "sender" ]; then
   for node in $(cat nodes); do
     let port="$port_base + $cnt"
     #ip="$ip_base.$cnt"
-    if [ $cnt -eq 1 ]; then
+    if [[ $cnt -eq $num_senders ]]; then
+      sleep 1
       output="$out_dir/lat_result_$node.txt"
-      cmd="$ib_write_lat -F -d $ib_dev -x $gidx -s $lat_size -n $lat_iter -l 1 -t 1 -p $port $ip_dst > $output"
+      cmd="$ib_write_lat -F -d $ib_dev -x $gidx -s $lat_size -n $lat_iter -l 1 -t 1 -p $port $ip_dst |tee $output"
     else
       output="$out_dir/bw_result_$node.txt"
       log="$out_dir/bw_log_$node.txt"
-      cmd="$ib_write_bw -F -e -d $ib_dev -x $gidx -s $bw_size -n $bw_iters -l 1 -t 1 -p $port $ip_dst -L $log > $output"
+      cmd="$ib_write_bw -F -e -d $ib_dev -x $gidx -s $bw_size -n $bw_iters -l 1 -t 1 -p $port $ip_dst -L $log |tee $output"
     fi
     echo "On $node: execute $cmd"
     ssh -o "StrictHostKeyChecking no" -p 22 $node $cmd &
     let cnt="$cnt + 1"
     #((++cnt))
   done
+  wait
+  echo "DONE"
 elif [ $1 == "receiver" ]; then
   for node in $(cat nodes); do
     let port="$port_base + $cnt"
-    if [ $cnt -eq 1 ]; then
-      cmd="$ib_write_lat -F -d $ib_dev -x $gidx -s $lat_size -n $lat_iter -l 1 -t 1 -p $port"
+    if [[ $cnt -eq $num_senders ]]; then
+      cmd="$ib_write_lat -F -d $ib_dev -x $gidx -s $lat_size -n $lat_iter -l 1 -t 1 -p $port &> /dev/null"
     else
-      cmd="$ib_write_bw -F -e -d $ib_dev -x $gidx -s $bw_size -n $bw_iters -l 1 -t 1 -p $port"
+      cmd="$ib_write_bw -F -e -d $ib_dev -x $gidx -s $bw_size -n $bw_iters -l 1 -t 1 -p $port &> /dev/null"
     fi
-    echo "On $node: execute $cmd"
-    ssh -o "StrictHostKeyChecking no" -p 22 $node $cmd &
+    echo "On $receiver: execute $cmd"
+    ssh -o "StrictHostKeyChecking no" -p 22 $receiver $cmd &
     let cnt="$cnt + 1"
   done
+  echo "DONE"
 else
   echo "Usage: bash $0.sh <sender/receiver>"
   exit
 fi
-wait
-echo "DONE"
