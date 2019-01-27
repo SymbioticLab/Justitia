1b3a18cb9d7fd1567940538d25c5fe0c44b2b7a0
Yiwen Zhang <yiwenzhg@umich.edu>
2019-01-27 08:12:09 -0500

    update kill proc script

--- scripts/kill_proc.sh
+++ scripts/kill_proc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # assume there is a file called "nodes" in cmd which includes all the node hostname
-proc="lock_simulation"
+proc="pacer"
 for node in $(cat nodes); do
   echo "killing $proc process in $node..."
   pids=`ssh -o 'StrictHostKeyChecking no' -p 22 $node ps -ef | grep $proc | awk '{print $2}'`
