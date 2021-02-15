3200d5768b176d6a692729ed659aad2571369fa3
Yiwen Zhang <yiwenzhg@umich.edu>
2021-02-15 01:06:00 -0500

    improve compiler X3 script

--- scripts/compile_X3_driver.sh
+++ scripts/compile_X3_driver.sh
@@ -2,7 +2,8 @@
 
 for node in $(cat nodes); do
     echo "compile and install X3 driver code in $node..."
-  ssh -o "StrictHostKeyChecking no" -p 22 $node "cp -r /proj/gaia-PG0/yiwen/frdma/libmlx4 /users/yiwenzhg/frdma/ && bash /users/yiwenzhg/frdma/compile_X3.sh" &
+    ssh -o "StrictHostKeyChecking no" -p 22 $node "cp -r /proj/gaia-PG0/yiwen/frdma/libmlx4 /users/yiwenzhg/frdma/ "
+    ssh -o "StrictHostKeyChecking no" -p 22 $node "bash -s" < ../compile_X3.sh
 done
 wait
 echo "DONE"
