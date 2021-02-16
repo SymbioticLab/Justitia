78f556f74c513013ac5d07b3118bdd4cbe438015
Yiwen Zhang <yiwenzhg@umich.edu>
2021-02-15 23:11:07 -0500

    better pacer compile script

--- scripts/compile_pacer.sh
+++ scripts/compile_pacer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for node in $(cat nodes); do
   echo "compile rdma_pacer in $node..."
-  ssh -o "StrictHostKeyChecking no" -p 22 $node "make -C frdma/rdma_pacer"
+  ssh -o "StrictHostKeyChecking no" -p 22 $node "cp -r /proj/gaia-PG0/yiwen/frdma/rdma_pacer /users/yiwenzhg/frdma/ && make clean -C frdma/rdma_pacer && make -C frdma/rdma_pacer"
 done
 wait
 echo "DONE"
