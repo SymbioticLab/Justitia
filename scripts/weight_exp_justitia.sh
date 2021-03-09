edf6b3c79fd0753143e2af6b4902672e27826a54
Yiwen Zhang <yiwenzhg@umich.edu>
2021-03-09 16:16:52 -0500

    update some scripts

--- scripts/weight_exp_justitia.sh
+++ scripts/weight_exp_justitia.sh
@@ -6,13 +6,16 @@ J_daemon=/users/yiwenzhg/frdma/rdma_pacer/pacer
 out_dir=/tmp
 num_justitia_flow=5    # the last one is a latency app; others are bw
 num_justitia_bw_flow=$((num_justitia_flow-1))
-sender_node=cpn1
-receiver_node=cpn2
+#sender_node=cpn1
+#receiver_node=cpn2
+sender_node=cpm1
+receiver_node=cpm2
 ip_dst=192.168.0.12     # hardcoded
 bw_size=1000000
 lat_size=16
-bw_iters=20000
-lat_iter=1000000
+bw_iters=60000
+lat_iter=10000000
+#lat_iter=100000000
 port_base=5000
 #cnt=1
 
@@ -48,11 +51,13 @@ echo "Justitia receiver DONE"
 for (( i=1; i<=$num_justitia_flow; i++ )); do
     let port="$port_base + $i"
     if [[ $i -eq $num_justitia_flow ]]; then
-        sleep 2
+        #sleep 2
+        sleep 1
         output="$out_dir/lat_result_weight_J_$sender_node.txt"
         cmd="export LD_LIBRARY_PATH=/usr/lib64; $ib_write_lat -F -s $lat_size -n $lat_iter --log_off -l 1 -t 1 -p $port $ip_dst |tee $output"
     else
-        sleep 1
+        #sleep 1
+        sleep 0.5
         output="$out_dir/bw_result_weight_J_$sender_node_$i.txt"
         log="$out_dir/bw_log_large_$sender_node_$i.txt"
         cmd="export LD_LIBRARY_PATH=/usr/lib64; $ib_write_bw -F -e -s $bw_size -n $bw_iters -l 1 -t 1 -p $port $ip_dst -L $log |tee $output"
