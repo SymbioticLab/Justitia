455577557dbcccb52923ce67aaf31e72022d8de4
Yiwen Zhang <yiwenzhg@umich.edu>
2021-09-20 19:41:10 -0400

    update repo name in scripts

--- setup_X3.sh
+++ setup_X3.sh
@@ -1,16 +1,16 @@
 sudo apt-get update
 sudo apt-get install -y pkg-config libnl-3-dev libnl-route-3-dev libibumad-dev librdmacm-dev
-cd ~/frdma/libibverbs-41mlnx1/
+cd ~/Justitia/libibverbs-41mlnx1/
 ./autogen.sh
 ./configure --prefix=/usr libdir=/usr/lib64
 make
 sudo make install
-cd ~/frdma/libmlx4
+cd ~/Justitia/libmlx4
 ./autogen.sh
 ./configure --prefix=/usr libdir=/usr/lib64
 make
 sudo make install
-cd ~/frdma/perftest-4.2/
+cd ~/Justitia/perftest-4.2/
 ./autogen.sh
 ./configure --prefix=/usr libdir=/usr/lib64
 make
